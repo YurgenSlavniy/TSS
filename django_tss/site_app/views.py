b1b837dfe4d8ac9bc15569d0f209441a9759b1df
YurgenSlavniy <80168826+YurgenSlavniy@users.noreply.github.com>
2023-11-23 22:20:30 +0300

    Update views.py

--- django_tss/site_app/views.py
+++ django_tss/site_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
-# Create your views here.
+# главная страница
+def index(request):
+	return render(request, 'index.html')
+# биржевые сводки по апиай получаемые 
+# и в красивом виде пользователю представленные
+def stock(request):
+    return render(request, "stock.html")
+# аналитика истори  торгов, загружаемой в формате .csv
+def history(request):
+    return render(request, "history.html")
+# калькулятор ордеров, расчитывает 
+# расстановку ордеров в зависимости от введёных пользователем параметров
+def calculator(request):
+    return render(request, "calculator.html")
+# Модели поведения рынка с расставленными ордерами, 
+# прогнозирование прибылей и убытков
+def models(request):
+    return render(request, "models.html")
